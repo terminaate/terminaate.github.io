460b7f1bb4bcf4cb222c92ba61556bf4e72f24ba
terminaate <tntwnik@gmail.com>
2022-10-09 12:31:28 +0300

    Change browser-router to hash-router

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>,
 );
