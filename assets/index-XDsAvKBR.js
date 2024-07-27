d7004c4d477bac7719ef61f7682bad59ff0c1c9c
terminaate <tntwnik@gmail.com>
2024-07-27 16:12:30 +0300

    Updates

--- assets/index-XDsAvKBR.js
+++
@@ -1,1 +0,0 @@
-import{j as a,P as s,T as e,p as c,a as d}from"./index-7YKfbWyg.js";const r="_projectsPage_d5ahd_1",j="_container_d5ahd_6",o="_projectContainer_d5ahd_18",t=()=>a.jsxs(s,{className:r,children:[a.jsx(e,{children:"Projects:"}),a.jsx("div",{className:j,children:c.map(((s,e)=>a.jsx(d,{className:o,project:s},e)))})]});export{t as default};
--- assets/index-cG41GyqW.js
+++
@@ -1,1 +0,0 @@
-import{j as s,P as o}from"./index-7YKfbWyg.js";const e=()=>s.jsx(o,{children:"I am sorry, something goes wrong, this page is not found"});export{e as default};
