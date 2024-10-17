4a7386774782cd57c3351246842915a41c48be9f
terminaate <tntwnik@gmail.com>
2024-10-17 12:56:14 +0300

    remove doMagic

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,11 @@ import { Compose } from '@/components/Compose';
 import { RoutingContextProvider } from '@/contexts/RoutingContext';
 import { ConfigContextProvider } from '@/contexts/ConfigContext';
 import { CursorContextProvider } from '@/contexts/CursorContext';
-import { doMagic } from '@/utils/doMagic';
 
-void doMagic();
-
+// @info: fixing touch hover event
 document.addEventListener('touchstart', function () {}, true);
-document.body.setAttribute('data-cursor', import.meta.env.DEV + '');
+
+document.body.setAttribute('data-cursor', String(import.meta.env.DEV));
 
 const root = createRoot(document.getElementById('root')!);
 
